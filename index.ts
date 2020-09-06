f5c86d1a1f441b5f507d382a7694eb65c455387e
Anwesh43 <anweshthecool0@gmail.com>
2020-09-06 15:56:29 +0530

    adding BTPNode class

--- index.ts
+++ index.ts
@@ -142,3 +142,45 @@ class Animator {
         }
     }
 }
+
+class BTPNode {
+
+    prev : BTPNode 
+    next : BTPNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length) {
+            this.next = new BTPNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawTPNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BTPNode {
+        var curr : BTPNode = this.prev 
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
